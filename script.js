4a16b5e69ac2435d16ee4dc209a3caf425ef89f1
Aidil-Maula <m.aidilmaula@gmail.com>
2022-12-11 01:33:45 +0800

    fifth update

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = document.querySelector(".itemleft__sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = 'Aidil';
+    }, 0);
+    setTimeout(() => {
+        text.textContent = 'Software Developer';
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = 'UI/UX Designer';
+    }, 8000);
+   
+}
+
+textLoad();
+setInterval(textLoad, 12000);
